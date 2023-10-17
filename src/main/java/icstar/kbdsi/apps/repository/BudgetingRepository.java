5e83be7a8f11f1ae054e18fe9b8ac5cd123e5e07
Luqmanul Hakim <64834791+lqmaan@users.noreply.github.com>
2023-10-17 14:08:52 +0700

    update src

---
+++ src/main/java/icstar/kbdsi/apps/repository/BudgetingRepository.java
@@ -0,0 +1,18 @@
+package icstar.kbdsi.apps.repository;
+
+import icstar.kbdsi.apps.models.Budgeting;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.logging.SocketHandler;
+
+public interface BudgetingRepository  extends JpaRepository<Budgeting, Long> {
+    List<Budgeting> findByYearAndIsDeleted(String year, boolean isDeleted);
+
+    Page<Budgeting> findByYearContainingAndIsDeleted(String year, boolean isDeleted, PageRequest pageable, Sort sort);
+
+
+}
