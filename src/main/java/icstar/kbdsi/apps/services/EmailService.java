171838b01a8b33fb426f3c5aa5ea752d7974a257
Luqmanul Hakim <64834791+lqmaan@users.noreply.github.com>
2023-10-17 14:06:19 +0700

    Delete src directory

--- src/main/java/icstar/kbdsi/apps/services/EmailService.java
+++
@@ -1,8 +0,0 @@
-package icstar.kbdsi.apps.services;
-
-import icstar.kbdsi.apps.dto.EmailDetailsDto;
-
-public interface EmailService {
-    String sendSimpleMail(EmailDetailsDto emailDetailsDto);
-    String sendMailWithAttachment(EmailDetailsDto emailDetailsDto);
-}
