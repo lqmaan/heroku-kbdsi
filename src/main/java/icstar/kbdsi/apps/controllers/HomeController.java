f3fccaba698277ac908dfa7d15cb6d8b35da8006
Luqmanul Hakim <64834791+lqmaan@users.noreply.github.com>
2023-10-20 17:19:32 +0700

    Update HomeController.java

--- src/main/java/icstar/kbdsi/apps/controllers/HomeController.java
+++ src/main/java/icstar/kbdsi/apps/controllers/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = {"http://localhost:4200", "http://https://kbdsi-icstar-fe.vercel.app/", "https://kbdsi-icstar-g0cg82eie-lh007lucky-gmailcom.vercel.app/" })
+@CrossOrigin(origins = {"http://localhost:4200", "https://kbdsi-icstar-fe.vercel.app/", "https://kbdsi-icstar-g0cg82eie-lh007lucky-gmailcom.vercel.app/" })
 
 
 @RestController
