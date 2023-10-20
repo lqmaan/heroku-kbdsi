16cc6be95e1c826b60f3436f25e9fcdfb6acedeb
Luqmanul Hakim <64834791+lqmaan@users.noreply.github.com>
2023-10-20 17:20:19 +0700

    Update CategoryController.java

--- src/main/java/icstar/kbdsi/apps/controllers/CategoryController.java
+++ src/main/java/icstar/kbdsi/apps/controllers/CategoryController.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.Optional;
 
 
-@CrossOrigin(origins = {"http://localhost:4200", "http://https://kbdsi-icstar-fe.vercel.app/", "https://kbdsi-icstar-g0cg82eie-lh007lucky-gmailcom.vercel.app/" })
+@CrossOrigin(origins = {"http://localhost:4200", "https://kbdsi-icstar-fe.vercel.app/", "https://kbdsi-icstar-g0cg82eie-lh007lucky-gmailcom.vercel.app/" })
 
 @RestController
 @RequestMapping("/api")
